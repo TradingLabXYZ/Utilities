633d83566c96dc284eaecaa92ba146252594e3c7
dolphinxyz <robimalco@gmail.com>
2022-02-27 13:08:40 +0100

    save to file

--- extract_etherscan_txs/extract.py
+++ extract_etherscan_txs/extract.py
@@ -1,15 +1,47 @@
 import time
+import numpy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-def parse_txs(html):
+address = "0x1317c62f99bde9bf41dcc0381b5cad04cdd50bf2"
+pages = 1
+
+def extract_links(driver):
+    links = []
+    tx_url = "https://etherscan.io/tokentxns?a={0}&p={1}"
+    for page in range(pages):
+        temp_tx_url = tx_url.format(address, str(page + 1))
+        print("Extracting", temp_tx_url, "...") 
+        driver.get(temp_tx_url)
+        time.sleep(3)
+        temp_links = driver.find_elements(By.TAG_NAME, "a")
+        for link in temp_links:
+            raw_link = str(link.get_attribute("href"))
+            if "/tx/" in raw_link:
+                links.append(raw_link)
+    return links
+
+def extract_txs(driver, links):
+    txs = []
+    for link in links:
+        print("\Analyzing", link, "...") 
+        driver.get(link)
+        time.sleep(3)
+        tx = parse_tx(driver.page_source)
+        print(tx)
+        txs.append(tx)
+    return txs
+
+
+def parse_tx(html):
     soup = BeautifulSoup(html, "html.parser")
     timestamp = extract_timestamp(soup)
-    swaps = extract_swap(timestamp, soup)
-    print(swaps)
+    info = extract_tx_info(timestamp, soup)
+    print("\t\tObtained", info) 
+    return info
 
 def extract_timestamp(soup):
     div_timestamp = soup.find("div", {"id": "ContentPlaceHolder1_divTimeStamp"})
@@ -18,51 +50,82 @@ def extract_timestamp(soup):
     timestamp = replace_month(timestamp)
     return timestamp.replace(" +UTC", "")
 
-def extract_swap(timestamp, soup):
-    swaps_details = get_swaps_from_soup(soup)
-    swaps_merged = merge_swaps(swaps_details)
-    swaps_cleaned = clean_swaps(timestamp, swaps_merged)
-    return swaps_cleaned
+def extract_tx_info(timestamp, soup):
+    info_details = get_info_from_soup(soup)
+    if info_details[0][0] == "Swap":
+        info_details = merge_swaps(info_details)
+        info_details = clean_swaps(timestamp, info_details)
+    if info_details[0][0] in ["liquidity", "Remove", "Add"]:
+        info_details = clean_liquidity(timestamp, info_details)
+    if info_details[0][0] == "From":
+        info_details = clean_transfer(timestamp, info_details)
+    return info_details
 
-def get_swaps_from_soup(soup):
-    swaps_details = []
+def get_info_from_soup(soup):
+    info_details = []
     div_sections = soup.find_all("div", {"class": "col-md-9"})
     for div_section in div_sections:
-        div_swaps = div_section.find_all("div", {"class": "media-body"})
-        for div_swap in div_swaps:
-            temp_swap_details = []
-            if div_swap:
-                spans_swap = div_swap.find_all(["span", "img", "a"])
-                for span_swap in spans_swap:
-                    if span_swap.text:
-                        temp_swap_details.append(span_swap.text)
-            if temp_swap_details[0] == "Swap":
-                swaps_details.append(temp_swap_details)
-    return swaps_details
+        div_infos = div_section.find_all("div", {"class": "media-body"})
+        for div_info in div_infos:
+            temp_info_details = []
+            if div_info:
+                spans_info = div_info.find_all(["span", "img", "a"])
+                for span_info in spans_info:
+                    if span_info.text:
+                        temp_info_details.append(span_info.text.strip())
+            if temp_info_details[0] in ["Swap", "liquidity", "Remove", "Add", "From"]:
+                info_details.append(temp_info_details)
+    return info_details
 
-def merge_swaps(swaps_details):
+def merge_swaps(info_details):
     i = 0
-    while i < len(swaps_details) - 1:
-        amount_now = swaps_details[i][-4]
-        amount_next = swaps_details[i + 1][2]
+    while i < len(info_details) - 1:
+        amount_now = info_details[i][-4]
+        amount_next = info_details[i + 1][2]
         if amount_now == amount_next:
-            new_swap = swaps_details[i] + swaps_details[i + 1]
-            swaps_details.pop(i + 1)
-            swaps_details[i] = new_swap
+            new_swap = info_details[i] + info_details[i + 1]
+            info_details.pop(i + 1)
+            info_details[i] = new_swap
         i = i + 1    
-    return swaps_details
+    return info_details
 
-def clean_swaps(timestamp, swaps_merged):
+def clean_swaps(timestamp, info_details):
     swaps_cleaned = []
-    for swap in swaps_merged:
+    for swap in info_details:
         from_qty = swap[2]
         from_pair = swap[3]
         to_qty = swap[-4]
         to_pair = swap[-3]
         swaps_cleaned.append([
-            timestamp, from_qty, from_pair, to_qty, to_pair
+            timestamp, "Swap", from_qty, from_pair, to_qty, to_pair
         ])
     return swaps_cleaned
+
+def clean_liquidity(timestamp, info_details):
+    liquidity_cleaned = []
+    for liquidity in info_details:
+        tx_type = liquidity[0]
+        qty1 = liquidity[1]
+        pair1 = liquidity[2]
+        qty2 = liquidity[4]
+        pair2 = liquidity[5]
+        liquidity_cleaned.append([
+            timestamp, tx_type, qty1, pair1, qty2, pair2
+        ])
+    return liquidity_cleaned
+
+def clean_transfer(timestamp, info_details):
+    transfer_cleaned = []
+    for transfer in info_details:
+        tx_type = "Transfer"
+        from_add = transfer[1]
+        to_add = transfer[5]
+        qty = transfer[9]
+        pair = transfer[11]
+        transfer_cleaned.append([
+            timestamp, tx_type, from_add, to_add, qty, pair
+        ])
+    return transfer_cleaned
 
 def replace_month(timestamp):
     months = {
@@ -83,20 +146,9 @@ def replace_month(timestamp):
         timestamp = timestamp.replace(month, months[month])
     return timestamp
 
-
 if __name__ == "__main__":
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://etherscan.io/tokentxns?a=0xd12d8b2f0480bf87d75a2559a397f486ed8eb026")
-    time.sleep(3)
-    temp_links = driver.find_elements(By.TAG_NAME, "a")
-    links = []
-    for link in temp_links:
-        raw_link = str(link.get_attribute("href"))
-        if "/tx/" in raw_link:
-            links.append(raw_link)
-    for link in links:
-        print("\n", link)
-        driver.get(link)
-        time.sleep(3)
-        parse_txs(driver.page_source)
+    links = extract_links(driver)
+    txs = extract_txs(driver, links)
     driver.quit()
+    numpy.savetxt('output.csv',a,delimiter=",")
--- extract_etherscan_txs/tx.py
+++ extract_etherscan_txs/tx.py
@@ -1,11 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-def main():
-    with open("tx.html") as fp:
-        soup = BeautifulSoup(fp, "html.parser")
+def parse_txs(html):
+    soup = BeautifulSoup(html, "html.parser")
     timestamp = extract_timestamp(soup)
-    swaps = extract_swap(timestamp, soup)
-    print(swaps)
+    info = extract_tx_info(timestamp, soup)
+    print(info)
 
 def extract_timestamp(soup):
     div_timestamp = soup.find("div", {"id": "ContentPlaceHolder1_divTimeStamp"})
@@ -14,51 +18,82 @@ def extract_timestamp(soup):
     timestamp = replace_month(timestamp)
     return timestamp.replace(" +UTC", "")
 
-def extract_swap(timestamp, soup):
-    swaps_details = get_swaps_from_soup(soup)
-    swaps_merged = merge_swaps(swaps_details)
-    swaps_cleaned = clean_swaps(timestamp, swaps_merged)
-    return swaps_cleaned
+def extract_tx_info(timestamp, soup):
+    info_details = get_info_from_soup(soup)
+    if info_details[0][0] == "Swap":
+        info_details = merge_swaps(info_details)
+        info_details = clean_swaps(timestamp, info_details)
+    if info_details[0][0] in ["Collect", "Remove", "Add"]:
+        info_details = clean_liquidity(timestamp, info_details)
+    if info_details[0][0] == "From":
+        info_details = clean_transfer(timestamp, info_details)
+    return info_details
 
-def get_swaps_from_soup(soup):
-    swaps_details = []
+def get_info_from_soup(soup):
+    info_details = []
     div_sections = soup.find_all("div", {"class": "col-md-9"})
     for div_section in div_sections:
-        div_swaps = div_section.find_all("div", {"class": "media-body"})
-        for div_swap in div_swaps:
-            temp_swap_details = []
-            if div_swap:
-                spans_swap = div_swap.find_all(["span", "img", "a"])
-                for span_swap in spans_swap:
-                    if span_swap.text:
-                        temp_swap_details.append(span_swap.text)
-            if temp_swap_details[0] == "Swap":
-                swaps_details.append(temp_swap_details)
-    return swaps_details
+        div_infos = div_section.find_all("div", {"class": "media-body"})
+        for div_info in div_infos:
+            temp_info_details = []
+            if div_info:
+                spans_info = div_info.find_all(["span", "img", "a"])
+                for span_info in spans_info:
+                    if span_info.text:
+                        temp_info_details.append(span_info.text.strip())
+            if temp_info_details[0] in ["Swap", "Collect", "Remove", "Add", "From"]:
+                info_details.append(temp_info_details)
+    return info_details
 
-def merge_swaps(swaps_details):
+def merge_swaps(info_details):
     i = 0
-    while i < len(swaps_details) - 1:
-        amount_now = swaps_details[i][-4]
-        amount_next = swaps_details[i + 1][2]
+    while i < len(info_details) - 1:
+        amount_now = info_details[i][-4]
+        amount_next = info_details[i + 1][2]
         if amount_now == amount_next:
-            new_swap = swaps_details[i] + swaps_details[i + 1]
-            swaps_details.pop(i + 1)
-            swaps_details[i] = new_swap
+            new_swap = info_details[i] + info_details[i + 1]
+            info_details.pop(i + 1)
+            info_details[i] = new_swap
         i = i + 1    
-    return swaps_details
+    return info_details
 
-def clean_swaps(timestamp, swaps_merged):
+def clean_swaps(timestamp, info_details):
     swaps_cleaned = []
-    for swap in swaps_merged:
+    for swap in info_details:
         from_qty = swap[2]
         from_pair = swap[3]
         to_qty = swap[-4]
         to_pair = swap[-3]
         swaps_cleaned.append([
-            timestamp, from_qty, from_pair, to_qty, to_pair
+            timestamp, "Swap", from_qty, from_pair, to_qty, to_pair
         ])
     return swaps_cleaned
+
+def clean_liquidity(timestamp, info_details):
+    liquidity_cleaned = []
+    for liquidity in info_details:
+        tx_type = liquidity[0]
+        qty1 = liquidity[1]
+        pair1 = liquidity[2]
+        qty2 = liquidity[4]
+        pair2 = liquidity[5]
+        liquidity_cleaned.append([
+            timestamp, tx_type, qty1, pair1, qty2, pair2
+        ])
+    return liquidity_cleaned
+
+def clean_transfer(timestamp, info_details):
+    transfer_cleaned = []
+    for transfer in info_details:
+        tx_type = "Transfer"
+        from_add = transfer[1]
+        to_add = transfer[5]
+        qty = transfer[9]
+        pair = transfer[11]
+        transfer_cleaned.append([
+            timestamp, tx_type, from_add, to_add, qty, pair
+        ])
+    return transfer_cleaned
 
 def replace_month(timestamp):
     months = {
@@ -79,5 +114,7 @@ def replace_month(timestamp):
         timestamp = timestamp.replace(month, months[month])
     return timestamp
 
+
 if __name__ == "__main__":
-    main()
+    with open("tx.html") as fp:
+        parse_txs(fp)
